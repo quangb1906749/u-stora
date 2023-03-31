6124b89a83ad14ddfb946e41eb6611c218a5ed72
Nguyen Thanh Quang <quangb1906749@student.ctu.edu.vn>
2023-03-31 17:17:38 +0700

    Chi tiet san pham

--- src/main/java/com/myproject/service/ICartService.java
+++ src/main/java/com/myproject/service/ICartService.java
@@ -10,6 +10,7 @@ import com.myproject.dto.CartDto;
 @Service
 public interface ICartService {
 	public HashMap<Long, CartDto> AddCart(long id, HashMap<Long, CartDto> cart);
+	public HashMap<Long, CartDto> AddCart(long id, int quanty, HashMap<Long, CartDto> cart);
 	public HashMap<Long, CartDto> EditCart(long id, int quanty, HashMap<Long, CartDto> cart);
 	public HashMap<Long, CartDto> DeleteCart(long id, HashMap<Long, CartDto> cart);
 	public int TotalQuanty( HashMap<Long, CartDto> cart);
